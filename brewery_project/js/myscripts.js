89a29470c132f644d74afe08c70b8507d6456584
Jonatan Roytgars <Yoni@Jonatans-MacBook-Air.local>
2016-08-24 10:30:24 -0400

    draft version 1.0

--- brewery_project/js/myscripts.js
+++ brewery_project/js/myscripts.js
@@ -4,27 +4,42 @@ $(document).ready(function(){
     $("#holambier").css("display", "block");
 
     $("h4").click(function() {
-      $(".description-box").css("display", "block");
+      $("#desc-box1").css("display", "block");
 
       $(".description-box span").click(function() {
         $(".description-box").css("display", "none");
-      })
-    })
+      });
+    });
   });
 
   $("#holambra_button").click(function(){
     $("#homepage").hide();
     $("#holambra").css("display", "block");
-  })
+
+    $("#sleep").click(function(){
+      window.open("https://ca.hotels.com/search.do?destination-id=1902171");
+    });
+
+    $("#btn2").click(function() {
+      $("#desc-box2").css("display", "block");
+
+      $(".description-box span").click(function() {
+        $(".description-box").css("display", "none");
+      });
+    });
+  });
 
   $("#bieren_button").click(function(){
     $("#homepage").hide();
     $("#bieren").css("display", "block");
-  })
+  });
 
   $("#contato_button").click(function(){
     $("#homepage").hide();
     $("#contato").css("display", "block");
-  })
 
+    $("#blog").click(function(){
+      window.open("https://holambier.wordpress.com/");
+    });
+  });
 });
